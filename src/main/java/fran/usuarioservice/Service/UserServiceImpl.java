27cc5077ace8933c7b1e11988968ada6a6e4e249
Fran <l.francisco.bj@gmail.com>
2024-08-30 16:42:49 -0600

    [FEATURE] Añadiendo implementacion de los metodos para el usuario

--- src/main/java/fran/usuarioservice/Service/UserServiceImpl.java
+++ src/main/java/fran/usuarioservice/Service/UserServiceImpl.java
@@ -1,2 +1,33 @@
-package fran.usuarioservice.Service;public class userServiceImpl {
+package fran.usuarioservice.Service;
+
+import fran.usuarioservice.Entity.User;
+import fran.usuarioservice.Exceptions.NotFoundException;
+import fran.usuarioservice.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public User saveUser(User user) {
+        String rndID = UUID.randomUUID().toString();
+        user.setUserID(rndID);
+        return userRepository.save(user);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User getUserById(String id) {
+        return userRepository.findById(id).orElseThrow(()-> new NotFoundException("User Not Found with id: " + id));
+    }
 }
