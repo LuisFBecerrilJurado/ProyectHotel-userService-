59cc86f9ee90edfd13cfaf410420bda19dc1df94
Fran <l.francisco.bj@gmail.com>
2024-08-30 16:42:24 -0600

    [FEATURE] Declarando los metodos para la entidad de usuario

--- src/main/java/fran/usuarioservice/Service/UserService.java
+++ src/main/java/fran/usuarioservice/Service/UserService.java
@@ -1,2 +1,11 @@
-package fran.usuarioservice.Service;public interface userService {
+package fran.usuarioservice.Service;
+
+import fran.usuarioservice.Entity.User;
+
+import java.util.List;
+
+public interface UserService {
+    User saveUser(User user);
+    List<User> getAllUsers();
+    User getUserById(String id);
 }
